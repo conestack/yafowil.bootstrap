2821f558fe0dec2b91bdd6a24d514aa7a939c26f
Robert Niederreiter <office@squarewave.at>
2012-06-22 12:24:16 +0200

    introduce bs_controls_renderer

--- src/yafowil/bootstrap/common.py
+++ src/yafowil/bootstrap/common.py
@@ -1,14 +1,38 @@
 from yafowil.base import factory
 from yafowil.compound import (
     hybrid_extractor,
+    compound_renderer,
     div_renderer,
 )
+from yafowil.utils import (
+    cssclasses,
+    css_managed_props,
+    managedprops,
+)
+
+
+@managedprops('id', *css_managed_props)
+def bs_controls_renderer(widget, data):
+    attrs = {
+        'id': widget.attrs.get('id'),
+        'class_': cssclasses(widget, data),
+    }
+    if len(widget):
+        rendered = compound_renderer(widget, data)
+    else:
+        rendered = data.rendered
+    if data.errors:
+        msgs = [error.message for error in data.errors]
+        rendered += data.tag('span', ', '.join(msgs), class_='help-inline')
+    if widget.attrs['help']:
+        rendered += data.tag('p', widget.attrs['help'], class_='help-block')
+    return data.tag('div', rendered, **attrs)
 
 
 factory.register(
     'bs_controls',
     extractors=[hybrid_extractor],
-    edit_renderers=[div_renderer],
+    edit_renderers=[bs_controls_renderer],
     display_renderers=[div_renderer])
 
 
@@ -32,6 +56,7 @@ BOOTSTRAP_MACROS = {
             'field.class_add': bs_field_error_class,
             'label.class': 'control-label',
             'bs_controls.class': 'controls',
+            'bs_controls.help': None,
         }
     },
     'button': {
