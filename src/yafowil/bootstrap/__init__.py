2842d65403d2951bcf629504bd049a460d8c172f
Robert Niederreiter <office@squarewave.at>
2012-09-28 10:35:03 +0200

    revert fanstatic attempt, later

--- src/yafowil/bootstrap/__init__.py
+++ src/yafowil/bootstrap/__init__.py
@@ -1,25 +1,17 @@
 import os
 from yafowil.base import factory
-try:
-    # try to provide as fanstatic resource
-    from js.bootstrap import bootstrap_responsive_css
-    def register():
-        import common
-        factory.register_theme('bootstrap', 'yafowil.bootstrap',
-                               css=[bootstrap_responsive_css])
-except ImportError:
-    # provide ourself if fanstatic not installed
-    resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
-    css = [{
-        'group': 'bootstrap',
-        'resource': os.path.join('css', 'bootstrap.css'),
-        'order': 10,
-    }, {
-        'group': 'bootstrap',
-        'resource': os.path.join('css', 'bootstrap-responsive.css'),
-        'order': 11,
-    }]
-    def register():
-        import common
-        factory.register_theme('bootstrap', 'yafowil.bootstrap',
-                               resourcedir, css=css)
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+css = [{
+    'group': 'bootstrap',
+    'resource': os.path.join('css', 'bootstrap.css'),
+    'order': 10,
+}, {
+    'group': 'bootstrap',
+    'resource': os.path.join('css', 'bootstrap-responsive.css'),
+    'order': 11,
+}]
+def register():
+    import common
+    factory.register_theme('bootstrap', 'yafowil.bootstrap',
+                           resourcedir, css=css)
