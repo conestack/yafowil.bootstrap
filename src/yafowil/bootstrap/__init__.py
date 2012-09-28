dabbf8e9c3f872d74d9fd63bf95f17066a0ffda2
Robert Niederreiter <office@squarewave.at>
2012-09-28 10:47:08 +0200

    blanklines

--- src/yafowil/bootstrap/__init__.py
+++ src/yafowil/bootstrap/__init__.py
@@ -1,6 +1,7 @@
 import os
 from yafowil.base import factory
 
+
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
 css = [{
     'group': 'bootstrap',
@@ -11,6 +12,8 @@ css = [{
     'resource': os.path.join('css', 'bootstrap-responsive.css'),
     'order': 11,
 }]
+
+
 def register():
     import common
     factory.register_theme('bootstrap', 'yafowil.bootstrap',
