84c2f2912c320fcdf101da2f9aeb293691c4c2ad
Jens W. Klein <jk@kleinundpartner.at>
2020-05-13 12:05:59 +0200

    use new yafowil

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'setuptools',
-        'yafowil>1.99',
+        'yafowil>3.99',
     ],
     tests_require=tests_require,
     extras_require = dict(
