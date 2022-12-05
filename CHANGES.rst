cab362a7d50eefd72b6d502677f986139aea3a18
Robert Niederreiter <rnix@squarewave.at>
2022-12-05 13:02:14 +0100

    Preparing release 1.3.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changes
 =======
 
-1.3.2 (unreleased)
+1.3.2 (2022-12-05)
 ------------------
 
 - Release wheel.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.3.2.dev0'
+version = '1.3.2'
 shortdesc = 'Bootstrap Styles for YAFOWIL'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
